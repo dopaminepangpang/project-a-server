98873655939ce48a535b0125f34257533f585198
신희성 <vvwv3092@gmail.com>
2025-09-19 23:01:20 +0900

    update: update redis host:

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 var ctx = context.Background()
-var rdb = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+var rdb = redis.NewClient(&redis.Options{Addr: "host.docker.internal:6379"})
 
 var roomsMu sync.Mutex
 var rooms = make(map[string]*Room)
